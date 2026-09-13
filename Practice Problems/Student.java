3ffd990b2e06659e126048e7988f6d551bd562ae
Adrija <adrijaroy906@gmail.com>
2026-09-13 19:18:45 +0530

    Student and College Information Management

---
+++ Practice Problems/Student.java
@@ -0,0 +1,25 @@
+class Student {
+    String name;
+    double attendance;
+
+    static String collegeName = "SRM Institute of Science and Technology";
+    static int studentCount = 0;
+
+    Student(String name, double attendance) {
+        this.name = name;
+        this.attendance = attendance;
+        studentCount++;
+    }
+
+    static void printCollegeInfo() {
+        System.out.println(collegeName);
+        System.out.println("Students created: " + studentCount);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Aditya", 85);
+        Student s2 = new Student("Ananya", 90);
+
+        Student.printCollegeInfo();
+    }
+}
